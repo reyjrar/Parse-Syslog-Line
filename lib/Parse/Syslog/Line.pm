35b8b77e00a48a551ab12d4e1477a948d5e27b51
Brad Lhotsky <brad@divisionbyzero.net>
2015-01-04 15:05:53 +0100

    Fixing the date handling logic in the tests to prevent installation failures.

--- lib/Parse/Syslog/Line.pm
+++ lib/Parse/Syslog/Line.pm
@@ -10,7 +10,7 @@ use Const::Fast;
 use DateTime::Format::HTTP;
 use HTTP::Date;
 
-our $VERSION        = '2.8';
+our $VERSION        = '2.9';
 
 our $DateTimeCreate    = 1;
 our $ExtractProgram    = 1;

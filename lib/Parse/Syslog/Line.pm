7ffb8be9dcb0bdaeeb3324a22a162221291683fe
Brad Lhotsky <brad@divisionbyzero.net>
2015-02-24 14:39:13 +0100

    Preparing the 3.0 release.

--- lib/Parse/Syslog/Line.pm
+++ lib/Parse/Syslog/Line.pm
@@ -10,7 +10,7 @@ use Const::Fast;
 use DateTime::Format::HTTP;
 use HTTP::Date;
 
-our $VERSION        = '2.9';
+our $VERSION        = '3.0';
 
 our $DateTimeCreate    = 1;
 our $ExtractProgram    = 1;

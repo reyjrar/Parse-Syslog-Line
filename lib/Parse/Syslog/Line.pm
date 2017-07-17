47382a670e105bae399d01ed68ec82fb4c4dc2d8
Brad Lhotsky <brad@divisionbyzero.net>
2017-07-17 06:52:15 +0200

    Version 4.0 to fix broken 3.9 upload.

--- lib/Parse/Syslog/Line.pm
+++ lib/Parse/Syslog/Line.pm
@@ -14,7 +14,7 @@ use Module::Load   qw( load );
 use Module::Loaded qw( is_loaded );
 use POSIX          qw( strftime tzset );
 
-our $VERSION = '3.9';
+our $VERSION = '4.0';
 
 # Default for Handling Parsing
 our $DateParsing     = 1;

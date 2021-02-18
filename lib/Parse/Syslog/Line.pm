1e2b67a489bd9852e457c614a7eb0a1a1c76027e
Brad Lhotsky <brad@divisionbyzero.net>
2021-02-18 14:27:34 -0800

    Tag the 4.6 release

--- lib/Parse/Syslog/Line.pm
+++ lib/Parse/Syslog/Line.pm
@@ -15,7 +15,7 @@ use Module::Loaded qw( is_loaded );
 use POSIX          qw( strftime tzset );
 use Ref::Util      qw( is_arrayref );
 
-our $VERSION = '4.5';
+our $VERSION = '4.6';
 
 # Default for Handling Parsing
 our $DateParsing     = 1;
